5de83de10477e176fbc62d246ba2a0153c065be7
shubhrajyotidey <toshubhrajyotidey@gmail.com>
2022-02-03 18:18:09 +0530

    Added comment funcitonality

--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -1,21 +1,45 @@
 import React, { useState } from "react";
 import styles from "./Post.module.css";
-import { InlineShareButtons } from "sharethis-reactjs";
-
 import ShowMoreText from "react-show-more-text";
-import User from "../../assets/img/default-user.png";
+
+/*-----MUI-----*/
+import Box from "@mui/material/Box";
+import { TextField } from "@mui/material";
+
 import ImageViewer from "../ImageViewer/ImageViewer";
+import User from "../../assets/img/default-user.png";
 
 import NotLiked from "../../assets/img/not-liked.png";
 import Liked from "../../assets/img/liked.png";
 import { APIService } from "../../services/api/api.service";
 import { API_METHODS } from "../../constants/api";
+import Spinner from "../spinners/Spinner";
 
-function Post({ post, liked, id }) {
+import {
+  LinkedinShareButton,
+  TelegramShareButton,
+  TwitterShareButton,
+  WhatsappShareButton,
+} from "react-share";
+import {
+  LinkedinIcon,
+  TelegramIcon,
+  TwitterIcon,
+  WhatsappIcon,
+} from "react-share";
+
+function Post({ post, liked, id, comments }) {
+  const url = window.location.href;
+  console.log(url);
   const api = new APIService();
   const [like, setLike] = useState(liked);
   const [likeCount, setLikeCount] = useState(post.likeCount);
   const [activityId, setActivityId] = useState(id);
+  const [commentList, setCommentList] = useState(comments);
+  const [comment, setComment] = useState("");
+  const [commentError, setCommentError] = useState(false);
+  const [commentText, setCommentText] = useState("");
+  const [loading, setLoading] = useState(false);
 
   const handleLike = () => {
     if (!like) {
@@ -48,9 +72,32 @@ function Post({ post, liked, id }) {
     }
   };
 
-  console.log(post);
+  const handleComment = () => {
+    if (comment) {
+      setLoading(true);
+      api
+        .call(API_METHODS.POST, "/api/posts/comment", {
+          headers: { postid: post._id },
+          body: { postId: post._id, message: comment },
+        })
+        .then((res) => {
+          if (res.data) {
+            console.log(res);
+            setCommentError(false);
+            setComment("");
+            setCommentText("");
+            setCommentList((comments) => [...comments, res.data]);
+          }
+          setLoading(false);
+        });
+    } else {
+      setCommentError(true);
+    }
+  };
+
   return (
     <React.Fragment>
+      {loading && <Spinner />}
       <div className={styles.Card}>
         <div className={styles.Heading}>
           <img
@@ -72,8 +119,8 @@ function Post({ post, liked, id }) {
         </div>
 
         <div className={styles.Text}>
-          <h2>{post.title}</h2>
-          <h4>
+          <h1>{post.title}</h1>
+          <h3>
             <ShowMoreText
               /* Default options */
               lines={3}
@@ -87,7 +134,7 @@ function Post({ post, liked, id }) {
             >
               {post.description}
             </ShowMoreText>
-          </h4>
+          </h3>
         </div>
         <div
           style={{
@@ -140,38 +187,109 @@ function Post({ post, liked, id }) {
               </h3>
             </div>
           )}
-          <InlineShareButtons
-            config={{
-              alignment: "center", // alignment of buttons (left, center, right)
-              color: "white", // set the color of buttons (social, white)
-              enabled: true, // show/hide buttons (true, false)
-              font_size: 16, // font size for the buttons
-              labels: "cta", // button labels (cta, counts, null)
-              language: "en", // which language to use (see LANGUAGES)
-              networks: [
-                // which networks to include (see SHARING NETWORKS)
-                "whatsapp",
-                "linkedin",
-                "messenger",
-                "facebook",
-                "twitter",
-              ],
-              padding: 12, // padding within buttons (INTEGER)
-              radius: 4, // the corner radius on each button (INTEGER)
-              // show_total: true,
-              size: 40, // the size of each button (INTEGER)
-
-              // OPTIONAL PARAMETERS
-              // url: "http://localhost:3000/post/61faeba5b8081910b0c74b14", // (defaults to current url)
-              // image: "https://bit.ly/2CMhCMC", // (defaults to og:image or twitter:image)
-              // description: "custom text", // (defaults to og:description or twitter:description)
-              // title: "custom title", // (defaults to og:title or twitter:title)
-              // message: "custom email text", // (only for email sharing)
-              // subject: "custom email subject", // (only for email sharing)
-              // username: "custom twitter handle", // (only for twitter sharing)
-            }}
-          />
+          <div style={{ display: "flex", gap: "10px" }}>
+            <LinkedinShareButton
+              children={<LinkedinIcon round size={45} />}
+              url={url}
+            />
+            <WhatsappShareButton
+              children={<WhatsappIcon round size={45} />}
+              url={url}
+            />
+            <TelegramShareButton
+              children={<TelegramIcon round size={45} />}
+              url={url}
+            />
+            <TwitterShareButton
+              children={<TwitterIcon round size={45} />}
+              url={url}
+            />
+          </div>
         </div>
+        <div style={{ marginTop: "20px" }}>
+          <h1 style={{ color: "rgb(17,143,133)" }}>
+            {comments.length} Comment{comments.length === 1 ? "" : "s"}
+          </h1>
+          <Box
+            component="form"
+            sx={{
+              "& > :not(style)": {
+                m: 1,
+                width: "100%",
+                borderBottom: "1px solid #66FCF1",
+              },
+              "& label.Mui-focused": {
+                color: "#1f2833",
+              },
+              "& .MuiInput-underline:after": {
+                borderBottomColor: "#1f2833",
+              },
+            }}
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              width: "100%",
+            }}
+            noValidate
+            autoComplete="off"
+          >
+            <TextField
+              id="filled-multiline-flexible"
+              label="Comment"
+              multiline
+              maxRows={4}
+              value={comment}
+              onChange={(e) => setComment(e.target.value)}
+              variant="filled"
+            />
+          </Box>
+          <div style={{ textAlign: "right" }}>
+            <div className={styles.Comment} onClick={handleComment}>
+              COMMENT
+            </div>
+          </div>
+        </div>
+        {commentList.length > 0 &&
+          commentList.map((comment, index) => {
+            return (
+              <div
+                className={`${styles.Heading} ${styles.Comments}`}
+                key={`Comment_${index}`}
+              >
+                <img
+                  src={post.userId.image || User}
+                  alt="user"
+                  style={{ height: "40px", marginRight: "10px" }}
+                />
+                <div
+                  style={{
+                    borderLeft: "2px solid rgb(17,143,133)",
+                    paddingLeft: "10px",
+                    display: "flex",
+                    flexDirection: "column",
+                    width: "100%",
+                  }}
+                >
+                  <h2>{post.userId.username}</h2>
+                  <h3>
+                    <ShowMoreText
+                      /* Default options */
+                      lines={3}
+                      more="Show more"
+                      less="Show less"
+                      className="content-css"
+                      anchorClass="my-anchor-css-class"
+                      expanded={false}
+                      // width={280}
+                      truncatedEndingComponent={"... "}
+                    >
+                      {comment.message}
+                    </ShowMoreText>
+                  </h3>
+                </div>
+              </div>
+            );
+          })}
       </div>
     </React.Fragment>
   );
